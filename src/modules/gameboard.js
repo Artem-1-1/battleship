4036eda2d8903401593c29fb77f7d5e1625f5013
Artem-1-1 <aleksejchukartem@gmail.com>
2025-11-27 14:09:23 +0300

    Added Ship/Gameboard/Player classes in modules dir.

---
+++ src/modules/gameboard.js
@@ -0,0 +1,83 @@
+import Ship from "./ship.js";
+
+export default class Gameboard {
+  constructor() {
+    this.ships = [];
+    this.missedAttacks = [];
+    this.shipPositions = new Map();
+    this.allCoordinates = new Set();
+  }
+
+  placeShip(length, [startX, startY], direction) {
+    if (startX < 0 || startX > 9 || startY < 0 || startY > 9) {
+      throw new Error('Ship placement out of bounds');
+    }
+
+    const coordinates = [];
+    const occupiedCheck = [];
+
+    for (let i = 0; i < length; i++) {
+      let x, y;
+      if (direction === 'horizontal') {
+        x = startX;
+        y = startY + i;
+      } else {
+        x = startX + i;
+        y = startY;
+      }
+
+      if (x > 9 || y > 9) {
+        throw new Error('Ship placement out of bounds');
+      }
+
+      coordinates.push([x, y]);
+
+      for (let dx = -1; dx <= 1; dx++) {
+        for (let dy = -1; dy <= 1; dx++) {
+          const checkX = x + dx;
+          const checkY = y + dy;
+          if (checkX >= 0 && checkX <= 9 && checkY >= 0 && checkY <= 9) {
+            occupiedCheck.push(`${checkX},${checkY}`)
+          }
+        }
+      }
+    }
+
+    for (const coord of occupiedCheck) {
+      if (this.allCoordinates.has(coord)) {
+        throw new Error('Ships must have at least 1 cell spacing between them');
+      }
+    }
+
+    const ship = new Ship();
+    this.ships.push(ship);
+    this.shipPositions.set(ship, coordinates);
+
+    coordinates.forEach(([x, y]) => {
+      this.allCoordinates.add(`${x},${y}`);
+    });
+
+    return ship;
+  }
+
+  receiveAttack([x, y]) {
+    for (const [ship, coordinates] of this.shipPositions) {
+      for (const [shipX, shipY] of coordinates) {
+        if (shipX === x && shipY === y) {
+          ship.hit();
+          return true;
+        }
+      }
+    }
+    this.missedAttacks.push([x, y]);
+    return false;
+  }
+
+  allShipsSunk() {
+    return this.ships.length > 0 && this.ships.every(ship => ship.isSunk());
+  }
+
+  getShipCoordinates(ship) {
+    return this.shipPositions.get(ship) || [];
+  }
+}
---
+++ src/modules/player.js
@@ -0,0 +1,51 @@
+import Gameboard from "./gameboard";
+
+export default class Player {
+  constructor(type) {
+    this.type = type;
+    this.gameboard = new Gameboard;
+    this.previousAttacks = new Set();
+  }
+
+  makeRandomAttack() {
+    if (this.previousAttacks.size >= 100) {
+      return null;
+    }
+    let x, y, attackKey;
+    let attempts = 0;
+    const maxAttempts = 200;
+
+    do {
+      x = Math.floor(Math.random() * 10);
+      y = Math.floor(Math.random() * 10);
+      attackKey = `${x},${y}`;
+      attempts++;
+
+      if (attempts > maxAttempts) {
+        return null;
+      }
+    } while (this.previousAttacks.has(attackKey));
+
+    this.previousAttacks.add(attackKey);
+    return [x, y];
+  }
+
+  makeAttack([x, y]) {
+    const attackKey = `${x},${y}`;
+
+    if (this.previousAttacks.has(attackKey)) {
+      throw new Error(`Already attacked this coordinate: ${x},${y}`);
+    }
+
+    this.previousAttacks.add(attackKey)
+    return [x, y];
+  }
+
+  hasAttacked([x, y]) {
+    return this.previousAttacks.has(`${x},${y}`);
+  }
+
+  resetAttacks() {
+    this.previousAttacks.clear();
+  }
+}
